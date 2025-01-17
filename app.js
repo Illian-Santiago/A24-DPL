5ba9d6c945918b49eb99d1bc0c079b48a89a3e73
Illian-Santiago <illianclase12345678@gmail.com>
2025-01-17 02:04:44 +0000

    Mejora codigo y tarea terminada

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// 1. Instancias
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,40 +8,35 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var favicon = require('serve-favicon');
 var path = require('path');
-
 var app = express();
 
-
-// view engine setup
+// 2. Configuraciones
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// 3. Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+// 4. Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-
-
-// catch 404 and forward to error handler
+// 5. Manejadores de errores
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+// 6. Exportación del servidor
 module.exports = app;
